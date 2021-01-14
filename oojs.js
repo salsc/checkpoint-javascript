c14814222cd7ef50288de7962d6d8e23390782bb
salsberyscottj <salsberyscottj@johndeere.com>
2021-01-14 09:21:13 -0600

    done

--- oojs.js
+++ oojs.js
@@ -6,7 +6,15 @@
 // - an `addSong` method that adds a song (string) to the `songs` array
 // Type your solution immediately below this line:
 
-
+class Playlist {
+  constructor(title) {
+    this.title = title;
+    this.songs = [];
+  }
+  addSong(aSong) {
+    this.songs.push(aSong);
+  }
+}
 
 
 
@@ -14,13 +22,14 @@
 // Call the instance's `addSong` method to add a song to the instance's `songs` array
 // Type your solution immediately below this line:
 
-
+var myPlaylist = new Playlist('My Playlist');
+myPlaylist.addSong('Oh Happy Day');
 
 
 
 // NOTE: THE CODE BELOW IS FOR TESTING PURPOSES. DO NOT REMOVE OR ALTER.
-if(typeof Playlist !== 'undefined') {
-  module.exports = {
-    Playlist
-  }
-}
+// if(typeof Playlist !== 'undefined') {
+//   module.exports = {
+//     Playlist
+//   }
+// }
